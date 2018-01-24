bd8e3a86a625f620800d7386a60fccf797b9cbb1
Paul Palayur <paulpalayur@gmail.com>
2018-01-25 00:41:59 +1100

    new file to project

---
+++ myCoinStat/DAL/myCoinStatInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using myCoinStat.Models;
+
+namespace myCoinStat.DAL
+{
+    public class myCoinStatInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<myCoinStatContext>
+    {
+        protected override void Seed(myCoinStatContext context)
+        {
+        }
+    }
+}
