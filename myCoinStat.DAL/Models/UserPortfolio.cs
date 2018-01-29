4ac20512a28883f81d5f1f3b92550cf920a3a804
Paul Palayur <paulpalayur@gmail.com>
2018-01-29 23:01:39 +1100

    adding files to Model

---
+++ myCoinStat.DAL/Models/UserPortfolio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myCoinStat.Models
+{
+    public class UserPortfolio
+    {
+        public int ID { get; set; }
+        public int PortfolioID { get; set; }
+        public int UserID { get; set; }
+
+        public virtual Portfolio Portfolio { get; set; }
+        public virtual User User { get; set; }
+    }
+}
